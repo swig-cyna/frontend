117fc55d7ca4fff055da0b3a412e03b840cb907a
Ralex <95540504+Ralex91@users.noreply.github.com>
2025-03-31 19:21:55 +0200

    feat(carousel): add carousel page

---
+++ src/app/admin/carousel/page.jsx
@@ -0,0 +1,13 @@
+import { DashboardHeader } from "@/features/admin/components/DashboardHeader"
+
+export default function CarouselPage() {
+  return (
+    <>
+      <DashboardHeader
+        heading="Carousel Management"
+        text="Config main carousel settings"
+      ></DashboardHeader>
+      <div className="p-6"></div>
+    </>
+  )
+}
