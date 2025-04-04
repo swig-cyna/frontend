5d2ead2bbfbaa04267cb08049fbe15b94b81ca4a
Ralex91 <95540504+Ralex91@users.noreply.github.com>
2025-04-04 13:51:27 +0200

    chore(lint): fix lint in shadcn ui components

--- src/components/ui/skeleton.jsx
+++ src/components/ui/skeleton.jsx
@@ -8,7 +8,8 @@ function Skeleton({
     (<div
       className={cn("animate-pulse rounded-md bg-primary/10", className)}
       {...props} />)
-  );
+  )
 }
 
 export { Skeleton }
+
--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -1,9 +1,7 @@
+import { cn } from "@/utils/functions"
 import * as React from "react"
 
-import { cn } from "@/utils/functions"
-
-const Textarea = React.forwardRef(({ className, ...props }, ref) => {
-  return (
+const Textarea = React.forwardRef(({ className, ...props }, ref) => (
     (<textarea
       className={cn(
         "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
@@ -11,8 +9,8 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
       )}
       ref={ref}
       {...props} />)
-  );
-})
+  ))
 Textarea.displayName = "Textarea"
 
 export { Textarea }
+
