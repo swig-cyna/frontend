fc8f321da5a2de0029585b6fb7169d167d6ace7c
dissemblable <julesvallet23@gmail.com>
2025-04-27 13:36:54 +0200

    feat(admin): add deleteSubscriptionDialog, subscriptionEdit and subscriptionList

---
+++ src/features/subscriptions/components/SubscriptionEdit.jsx
@@ -0,0 +1,155 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { createPlantSchema } from "@/features/subscriptions/schemas/createPlant"
+import { zodResolver } from "@hookform/resolvers/zod"
+import MDEditor from "@uiw/react-md-editor"
+import { useForm } from "react-hook-form"
+
+export const SubscriptionEdit = ({ plant, onSave, isLoading }) => {
+  const form = useForm({
+    resolver: zodResolver(createPlantSchema),
+    defaultValues: {
+      name: plant?.name || "",
+      description: plant?.description || "",
+      interval: plant?.interval || "month",
+      discount: plant?.discount || 0,
+      price: plant?.price || 0,
+    },
+  })
+  console.log(plant)
+
+  const onSubmit = (data) => {
+    onSave(data)
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Subscription name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex gap-4">
+            <div className="flex-1">
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        min={0}
+                        placeholder="100 €"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="flex-1">
+              <FormField
+                control={form.control}
+                name="interval"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Interval</FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select an interval" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="day">Day</SelectItem>
+                          <SelectItem value="week">Week</SelectItem>
+                          <SelectItem value="month">Month</SelectItem>
+                          <SelectItem value="year">Year</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="flex-1">
+            <FormField
+              control={form.control}
+              name="discount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Discount</FormLabel>
+                  <FormControl>
+                    <Input type="number" min={0} placeholder="5" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <MDEditor
+                    className="tesssss"
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end">
+            <Button type="submit" disabled={isLoading}>
+              {isLoading ? "Saving..." : "Save"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  )
+}
---
+++ src/features/subscriptions/components/SubscriptionsList.jsx
@@ -0,0 +1,193 @@
+"use client"
+
+import {
+  Edit,
+  Loader2,
+  LucideLoader2,
+  MoreHorizontal,
+  Package,
+  Plus,
+  Trash2,
+} from "lucide-react"
+import { useEffect, useState } from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { DeleteSubscriptionDialog } from "@/features/subscriptions/components/DeleteSubscriptionDialog"
+import { usePlants } from "@/features/subscriptions/hooks/usePlants"
+import { useDebounce } from "@uidotdev/usehooks"
+import { useRouter } from "next/navigation"
+
+export function SubscriptionList() {
+  const router = useRouter()
+  const [limit, setLimit] = useState(10)
+  const [search, setSearch] = useState("")
+  const [currentPage, setCurrentPage] = useState(1)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const [productToDelete, setProductToDelete] = useState(null)
+
+  const debouncedSearch = useDebounce(search, 300)
+
+  const { data: products, isLoading, isFetching } = usePlants()
+
+  console.log(products)
+
+  const handleCreateSubscription = () => {
+    router.push("/admin/subscriptions/new")
+  }
+
+  const handleEditProduct = (id) => () => {
+    router.push(`/admin/subscriptions/edit/${id}`)
+  }
+
+  const handleChangeLimit = (value) => {
+    setLimit(value)
+    setCurrentPage(1)
+  }
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+    setCurrentPage(1)
+  }
+
+  const handleDeleteProduct = (product) => () => {
+    setProductToDelete(product)
+    setIsDeleting(true)
+  }
+
+  useEffect(() => {
+    setCurrentPage(1)
+  }, [debouncedSearch])
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center">
+        <LucideLoader2 className="animate-spin" />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="mb-2 flex items-center justify-between">
+        <h2 className="text-lg font-semibold">Subscriptions</h2>
+
+        <div className="flex items-center justify-end gap-2">
+          {isFetching && <Loader2 className="h-6 w-6 animate-spin" />}
+          <Input
+            type="search"
+            placeholder="Search..."
+            className="w-40"
+            onChange={handleSearch}
+          />
+          <Select value={limit} onValueChange={handleChangeLimit}>
+            <SelectTrigger className="w-20">
+              <SelectValue placeholder="Select a limit" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value={5}>5</SelectItem>
+              <SelectItem value={10}>10</SelectItem>
+              <SelectItem value={20}>20</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Subscription</TableHead>
+            <TableHead className="text-right">Price</TableHead>
+            <TableHead className="text-right">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell className="font-medium">
+                <div>
+                  <div>{product.name}</div>
+                  <div className="text-xs text-muted-foreground">
+                    {product.id}
+                  </div>
+                </div>
+              </TableCell>
+              <TableCell className="text-right">
+                {product.price.toFixed(2)} €
+              </TableCell>
+              <TableCell className="text-right">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Actions</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={handleEditProduct(product.id)}>
+                      <Edit className="mr-2 h-4 w-4" />
+                      Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      onClick={handleDeleteProduct(product)}
+                      className="text-destructive"
+                    >
+                      <Trash2 className="mr-2 h-4 w-4" />
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      {products.length === 0 && (
+        <div className="mt-2 flex flex-col items-center justify-center rounded-md border border-dashed p-12">
+          <Package className="mb-4 h-12 w-12 text-muted-foreground" />
+          <p className="mb-4 text-center text-muted-foreground">
+            No subscriptions found. Add subscriptions to your store.
+          </p>
+
+          <Button onClick={handleCreateSubscription}>
+            <Plus className="mr-1 h-4 w-4" />
+            Add subscription
+          </Button>
+        </div>
+      )}
+
+      {productToDelete && (
+        <DeleteSubscriptionDialog
+          product={productToDelete}
+          open={isDeleting}
+          onOpenChange={(open) => !open && setProductToDelete(null)}
+        />
+      )}
+    </>
+  )
+}
